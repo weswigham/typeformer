4a34049ca3e7dd246f015e2327c0f35b2e49446c
Wesley Wigham <wewigham@microsoft.com>
2019-11-05 16:00:17 -0800

    Revise test structure

--- src/index.test.ts
+++ src/index.test.ts
@@ -3,14 +3,15 @@ import * as path from "path";
 import "./directorySnapshot";
 import {transformProjectAtPath} from "./";
 
-const fixturesDir = path.join(__dirname, "../test/fixtures");
+const testDir = path.join(__dirname, "../test")
+const fixturesDir = path.join(testDir, "fixtures");
 
 describe("the project transform", () => {
     const directoryResult = fs.readdirSync(fixturesDir);
     for (const dir of directoryResult) {
         it(`with ${dir}`, () => {
-            const baselineLocalDir = path.join(fixturesDir, dir, "baseline", "local");
-            const baselineReferenceDir = path.join(fixturesDir, dir, "baseline", "reference");
+            const baselineLocalDir = path.join(testDir, "baseline", "local", dir);
+            const baselineReferenceDir = path.join(testDir, "baseline", "reference", dir);
             transformProjectAtPath(path.join(fixturesDir, dir, "input", "tsconfig.json"), baselineLocalDir);
             expect(baselineLocalDir).toMatchDirectorySnapshot(baselineReferenceDir);
         });
