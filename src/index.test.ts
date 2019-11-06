e910ba71cbbb9dcea3a17416fc955fdf34e726ad
Wesley Wigham <wewigham@microsoft.com>
2019-11-05 16:06:46 -0800

    Rework tests into a useable thing

--- src/index.test.ts
+++ src/index.test.ts
@@ -5,13 +5,14 @@ import {transformProjectAtPath} from "./";
 
 const testDir = path.join(__dirname, "../test")
 const fixturesDir = path.join(testDir, "fixtures");
+const baselineDir = path.join(testDir, "baselines");
 
 describe("the project transform", () => {
     const directoryResult = fs.readdirSync(fixturesDir);
     for (const dir of directoryResult) {
         it(`with ${dir}`, () => {
-            const baselineLocalDir = path.join(testDir, "baseline", "local", dir);
-            const baselineReferenceDir = path.join(testDir, "baseline", "reference", dir);
+            const baselineLocalDir = path.join(baselineDir, "local", dir);
+            const baselineReferenceDir = path.join(baselineDir, "reference", dir);
             transformProjectAtPath(path.join(fixturesDir, dir, "input", "tsconfig.json"), baselineLocalDir);
             expect(baselineLocalDir).toMatchDirectorySnapshot(baselineReferenceDir);
         });
---
+++ test/baselines/reference/project-references-ns-prepend/cli/helpers.ts
@@ -0,0 +1,5 @@
+namespace ns {
+    export function execute() {
+        console.log(versionMajorMinor);
+    }
+}
