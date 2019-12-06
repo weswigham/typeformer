253896fab3e859ced5ca76add6c5d1ead407ea7d
Wesley Wigham <wewigham@microsoft.com>
2019-12-06 15:53:20 -0800

    transforming in place doesnt work because you overwrite config files as you build, which is unfortunate

--- src/cli.ts
+++ src/cli.ts
@@ -1,10 +1,12 @@
 #!/usr/bin/env node
-import { transformProjectInPlace } from ".";
+import { transformProjectFromNamespacesToModules } from ".";
 import { existsSync } from "fs";
-import { resolve } from "path";
+import { resolve, dirname } from "path";
 
 const fileName = process.argv[2];
 if (!fileName || !existsSync(fileName)) {
     console.error(`File ${fileName} not found - provide a path to the root project tsconfig.`);
+    process.exit(1);
 }
-transformProjectInPlace(resolve(process.cwd(), fileName));
+const configPath = resolve(process.cwd(), fileName);
+transformProjectFromNamespacesToModules(configPath, dirname(configPath)+"-new");
